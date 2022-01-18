042b539a48d166056f2ae4006bb4bcb174ad9682
unknown <dineshdkda31@gmail.com>
2022-01-18 20:35:31 +0530

    first commit

--- reunion/reunion/settings.py
+++ reunion/reunion/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gf$k9%njc6dz!6j7(^_$6h(k^$hz$$_$^46v0rk!@6!coqn-nv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
