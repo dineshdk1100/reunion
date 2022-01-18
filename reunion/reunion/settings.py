a8da6ead7529b7b07889d8091113445922a6a9a0
unknown <dineshdkda31@gmail.com>
2022-01-18 11:18:15 +0530

    message

--- reunion/reunion/settings.py
+++ reunion/reunion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gf$k9%njc6dz!6j7(^_$6h(k^$hz$$_$^46v0rk!@6!coqn-nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://reunionsocial.herokuapp.com']
 
