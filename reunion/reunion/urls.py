0c5a444cb5aec2468bf90b5e39013fa2758bf529
unknown <dineshdkda31@gmail.com>
2022-01-18 11:30:52 +0530

    message

--- reunion/reunion/urls.py
+++ reunion/reunion/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('socialmedia.urls')),
+    path('',include('reunion.socialmedia.urls')),
     path('admin/', admin.site.urls)
 ]
